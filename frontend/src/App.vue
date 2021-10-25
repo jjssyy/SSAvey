48c7698917bd1d921f89489f0a457dc3b06eef13
김대호 <olivedrab11@gmail.com>
2021-10-25 15:37:01 +0900

    Fix: vuetify 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,56 @@
 <template>
-  <div>
-    <header>
-      <!-- url 변경 -->
-      <router-link to="/login">로그인</router-link> |
-      <router-link to="/main">메인</router-link>
-    </header>
-    <!-- url변경될때 pagecomponent가 뜨는 영역 -->
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import '@/css/common/default.css'
-export default {}
-</script>
+import HelloWorld from './components/HelloWorld'
 
-<style></style>
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <v-row>
-      <v-col>
-        1
-      </v-col>
-    </v-row>
+    <h1>로그인페이지입니다.</h1>
+    <!-- <v-container>
+      <v-row>
+        <v-col>
+          1
+        </v-col>
+        <v-col>
+          2
+        </v-col>
+      </v-row>
+    </v-container> -->
   </div>
 </template>
 
