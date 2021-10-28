e541cbfad5ff112d8c88e620d266cbfa81af3d1b
전시영 <nnaoww@naver.com>
2021-10-28 15:39:09 +0900

    [S05P31C105-46][BE] feat: service 파일 추가

---
+++ backend/src/main/java/com/web/curation/member/MemberController.java
@@ -0,0 +1,15 @@
+package com.web.curation.member;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin
+@AllArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+}
---
+++ backend/src/main/java/com/web/curation/survey/SurveyService.java
@@ -0,0 +1,11 @@
+package com.web.curation.survey;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class SurveyService {
+}
