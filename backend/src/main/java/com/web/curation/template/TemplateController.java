9e8e5bf75c8cbf5dd6822fc1540fc637aa898d92
전시영 <nnaoww@naver.com>
2021-10-28 15:34:01 +0900

    [S05P31C105-46][BE] feat: controller 파일 추가

---
+++ backend/src/main/java/com/web/curation/template/TemplateController.java
@@ -0,0 +1,15 @@
+package com.web.curation.template;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin
+@AllArgsConstructor
+@RequestMapping("/template")
+public class TemplateController {
+}
