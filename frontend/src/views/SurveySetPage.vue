23fc6a9ff395c4d542d0da23b0253f66aa5b5ad7
김대호 <olivedrab11@gmail.com>
2021-11-01 15:26:03 +0900

    [S05P31C105-49][FE] fix: 수정

--- frontend/src/views/SurveySetPage.vue
+++ frontend/src/views/SurveySetPage.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="component-total">
     <!-- 설문 문항 -->
-    <div class="component-11">
-      haha
-    </div>
+    <div class="component-11" ref="left" id="display drop-target"></div>
     <!-- 오른쪽 사이드바 -->
-    <div class="component-22">
-      nana
+    <div class="component-22" ref="right" id="drag-elements">
+      <div>테스트1</div>
+      <div>테스트2</div>
+      <div>테스트3</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import dragula from 'dragula'
+
+export default {
+  mounted() {
+    const { left, right } = this.$refs
+
+    dragula([right, left], {
+      revertOnSpill: true,
+    })
+  },
+}
 </script>
 
 <style scoped src="./../css/survey/surveyset.css"></style>
