3ba3b71f7a3e190b61abbd45d327a148a21c2500
모진혁 <gkgk246@naver.com>
2021-11-02 14:45:38 +0900

    [S05P31C105-48][BE]feat:설문조사 관련 API구현

--- backend/src/main/java/com/web/curation/survey/SurveyController.java
+++ backend/src/main/java/com/web/curation/survey/SurveyController.java
@@ -5,11 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,4 +40,33 @@ public class SurveyController {
     	resultmap.put("data", "success");
     	return new ResponseEntity<>(resultmap,HttpStatus.OK); 
     }
+    
+    @GetMapping("/state/{state}")
+    public ResponseEntity<Map<String, Object>> getSurvey(
+    		@PathVariable String state,
+    		@RequestParam String uid
+    		){
+    	Map<String, Object> resultmap=new HashMap<String, Object>();
+    	List<Survey> result=surveyService.getSurvey(state, uid);
+    	
+    	resultmap.put("status", HttpStatus.OK);
+    	resultmap.put("data", result);
+    	return new ResponseEntity<>(resultmap,HttpStatus.OK); 	
+    }
+    
+    @GetMapping("/{survey_id}")
+    public ResponseEntity<Map<String, Object>> getSurveyInfo(
+    		@PathVariable String survey_id 
+    		){
+    	Map<String, Object> resultmap=new HashMap<String, Object>();
+    	Survey survey=surveyService.getSurveyInfo(survey_id);
+  
+    	
+    	resultmap.put("status", HttpStatus.OK);
+    	resultmap.put("data", survey);
+    	return new ResponseEntity<>(resultmap,HttpStatus.OK); 	
+    }
+    
+    
+    
 }
