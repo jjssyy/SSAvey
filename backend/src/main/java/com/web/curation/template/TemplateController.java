730157de4061f91b119c156b4a88e51bedc3947d
mev01 <mevolicus@gmail.com>
2021-11-02 14:35:36 +0900

    [S05P31C105-58][BE] feat : template api 관련

--- backend/src/main/java/com/web/curation/template/TemplateController.java
+++ backend/src/main/java/com/web/curation/template/TemplateController.java
@@ -1,10 +1,14 @@
 package com.web.curation.template;
 
+import com.web.curation.survey.Survey;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -14,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TemplateController {
 
     private TemplateService templateService;
+
+    @PostMapping()
+    public ResponseEntity<Map<String, Object>> createTemplate(@RequestBody Template template){
+        Map<String, Object> resultmap=new HashMap<String, Object>();
+
+        templateService.createTemplate(template);
+
+        resultmap.put("status", HttpStatus.OK);
+        resultmap.put("message", "success");
+        return new ResponseEntity<>(resultmap,HttpStatus.OK);
+    }
 }
