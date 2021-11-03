abde0d12e393bccda4a24c2b5086a184db43aaff
김대호 <olivedrab11@gmail.com>
2021-11-03 13:33:48 +0900

    [S05P31C105-65][FE] feat: 메인페이지

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,6 @@
 <template>
-<<<<<<< HEAD
   <v-app>
-    <header>
-      <!-- url 변경 -->
-      <router-link to="/login">로그인</router-link> |
-      <router-link to="/select">메인</router-link> |
-      <router-link to="/surveyedit">설문작성</router-link>
-    </header>
-    <!-- url변경될때 pagecomponent가 뜨는 영역 -->
-    <router-view></router-view>
-  </v-app>
-=======
-  <div>
-    <div v-if="isLogin">
+    <div id="v-app" v-if="isLogin">
       <nav>
         <AppHeader @openMenu="openMenu"></AppHeader>
       </nav>
@@ -29,8 +17,7 @@
         <router-view></router-view>
       </v-app>
     </div>
-  </div>
->>>>>>> 2c71696deb2e24afcd0d3005cf92d484fd1f64ee
+  </v-app>
 </template>
 
 <script>
