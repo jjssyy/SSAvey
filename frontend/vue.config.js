5f68265c0637a1a10b77f64852e2baa67d320ad8
양동현 <mevolicus@gmail.com>
2021-11-05 07:16:54 +0000

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  devServer: {
-    overlay: false,
+  configureWebpack: {
+    // other webpack options to merge in ...
   },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: { host: '0.0.0.0', hot: true, disableHostCheck: true },
 
   transpileDependencies: ['vuetify'],
 }
