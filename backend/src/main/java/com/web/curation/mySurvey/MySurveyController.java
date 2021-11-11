023d9c51ca7dfe95198aefc1b7e043699c2811fd
모진혁 <gkgk246@naver.com>
2021-11-11 14:10:28 +0900

    [S05P31C105-91][BE]페이징 처리 구현

--- backend/src/main/java/com/web/curation/mySurvey/MySurveyController.java
+++ backend/src/main/java/com/web/curation/mySurvey/MySurveyController.java
@@ -4,6 +4,7 @@ import com.web.curation.survey.State;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,13 +29,23 @@ public class MySurveyController {
 
 	private MySurveyService mySurveyService;
 	
-	@GetMapping("/{uid}/state/{state}")
-	public ResponseEntity<Map<String, Object>> getMySurvey( @PathVariable String uid, @PathVariable State state){
-
+	@GetMapping("/{uid}/state/{state}/{pageNumber}")
+	public ResponseEntity<Map<String, Object>> getMySurvey( 
+			@PathVariable String uid,
+			@PathVariable State state,
+			@PathVariable int pageNumber
+			){
 		Map<String, Object> resultMap=new HashMap<>();
 		List<MySurveyDto> mySurveyList = mySurveyService.getMySurveysByState(uid, state);
-
+		//페이징 처리
+		int maxSize=mySurveyList.size();
+		int start=pageNumber*10;
+		int end=start+10;
+		if(end>maxSize)end=maxSize;
+		mySurveyList=new ArrayList<MySurveyDto>(mySurveyList.subList(start, end));
+		resultMap.put("status", HttpStatus.OK);
 		resultMap.put("data", mySurveyList);
+		resultMap.put("Pagecount", maxSize/10+1);
 		if(mySurveyList.size()==0){
 			resultMap.put("message","설문 목록이 없습니다.");
 		}
