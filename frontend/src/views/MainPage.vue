22b48bf7b6d67103f074f1fccc32f5cd55299ce8
mev01 <mevolicus@gmail.com>
2021-11-11 17:42:13 +0900

    [S05P31C105-102][FE] fix : 회원가입 창 수정

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app style="background-color: #EEF2F7;">
+  <v-app style="background-color: #eef2f7">
     <v-carousel
       cycle
       height="400"
@@ -14,17 +14,17 @@
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
-    <v-container class="lighten-5" style="min-width: 100%;">
+    <v-container class="lighten-5" style="min-width: 100%">
       <v-row>
-        <v-col style="min-height: 300px;" cols="12" sm="6">
-          <div style="border-bottom: 2px solid;">
+        <v-col style="min-height: 300px" cols="12" sm="6">
+          <div style="border-bottom: 2px solid">
             <h2>
               진행 중인 설문 <i @click="gotoProceeding" class="fas fa-plus"></i>
             </h2>
           </div>
           <div
             class="d-flex justify-space-between"
-            style="margin: 3px 5px;"
+            style="margin: 3px 5px"
             v-if="proceedingSurvey.length <= 0"
           >
             진행 중인 설문이 없습니다.
@@ -33,10 +33,16 @@
             class="d-flex justify-space-between"
             v-for="(survey, index) in proceedingSurvey"
             :key="index"
-            style="padding: 5px; margin: 3px 5px; align-items: center; border: 1px solid; border-radius: 10px;"
+            style="
+              padding: 5px;
+              margin: 3px 5px;
+              align-items: center;
+              border: 1px solid;
+              border-radius: 10px;
+            "
           >
             <v-btn fab small class="mx-2" style="font-weight: bold">진행</v-btn>
-            <v-row class="d-flex" style="align-items: center;">
+            <v-row class="d-flex" style="align-items: center">
               <v-col cols="12" sm="6">
                 <h4>{{ survey.title }}</h4>
               </v-col>
@@ -49,25 +55,20 @@
             </v-row>
           </div>
         </v-col>
-        <v-col
-          @click="gotoExpected"
-          style="min-height: 300px;"
-          cols="12"
-          sm="6"
-        >
-          <div style="border-bottom: 2px solid;">
+        <v-col @click="gotoExpected" style="min-height: 300px" cols="12" sm="6">
+          <div style="border-bottom: 2px solid">
             <h2>진행 예정 설문</h2>
           </div>
           <div
             class="d-flex justify-space-between"
-            style="margin: 3px 5px;"
+            style="margin: 3px 5px"
             v-if="expectedSurvey.length <= 0"
           >
             진행 예정인 설문이 없습니다.
           </div>
           <div
             class="d-flex justify-space-between"
-            style="margin: 3px 5px;"
+            style="margin: 3px 5px"
             v-for="(survey, index) in expectedSurvey"
             :key="index"
           >
@@ -80,16 +81,16 @@
         </v-col>
         <v-col
           @click="gotoCompleted"
-          style="min-height: 300px;"
+          style="min-height: 300px"
           cols="12"
           sm="6"
         >
-          <div style="border-bottom: 2px solid;">
+          <div style="border-bottom: 2px solid">
             <h2>완료된 설문</h2>
           </div>
           <div
             class="d-flex justify-space-between"
-            style="margin: 3px 5px;"
+            style="margin: 3px 5px"
             v-if="completedSurvey.length <= 0"
           >
             완료된 설문이 없습니다.
@@ -98,7 +99,7 @@
             class="d-flex justify-space-between"
             v-for="(survey, index) in completedSurvey"
             :key="index"
-            style="margin: 3px 5px;"
+            style="margin: 3px 5px"
           >
             <h4>{{ survey.title }}</h4>
             <h5>
@@ -114,37 +115,39 @@
 
 <script>
 // import main from '@/api/main.js'
+import UserApi from "@/api/UserApi";
+import axios from "axios";
 export default {
   data() {
     return {
       colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
       ],
-      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
       expectedSurvey: [],
       proceedingSurvey: [
         {
-          title: '첫번째 설문',
-          start_date: '2021.11.01 18:00',
-          end_date: '2021.11.02.09:00',
+          title: "첫번째 설문",
+          start_date: "2021.11.01 18:00",
+          end_date: "2021.11.02.09:00",
         },
         {
-          title: '첫번째 설문',
-          start_date: '2021.11.01 18:00',
-          end_date: '2021.11.02.09:00',
+          title: "첫번째 설문",
+          start_date: "2021.11.01 18:00",
+          end_date: "2021.11.02.09:00",
         },
         {
-          title: '첫번째 설문',
-          start_date: '2021.11.01 18:00',
-          end_date: '2021.11.02.09:00',
+          title: "첫번째 설문",
+          start_date: "2021.11.01 18:00",
+          end_date: "2021.11.02.09:00",
         },
       ],
       completedSurvey: [],
-    }
+    };
   },
   // async created() {
   //   main.getSurveysByStatus(
@@ -163,16 +166,41 @@ export default {
   // },
   methods: {
     gotoExpected() {
-      this.$router.push('/survey/state/expected')
+      this.$router.push("/survey/state/expected");
     },
     gotoProceeding() {
-      this.$router.push('/survey/state/proceeding')
+      this.$router.push("/survey/state/proceeding");
     },
     gotoCompleted() {
-      this.$router.push('/survey/state/completed')
+      this.$router.push("/survey/state/completed");
     },
   },
-}
+  created() {
+    UserApi.userInfo(
+      {
+        uid: this.$store.state.uid,
+      },
+      (res) => {
+        this.$store.commit("setUser", res.data.user);
+      },
+      (err) => {
+        console.log(err);
+        alert("올바르지 않은 UID입니다.");
+      }
+    );
+    axios
+      .create({
+        baseURL: "https://meeting.ssafy.com/api/v4",
+        headers: {
+          Authorization: "Bearer uieydcqsspf87n6d7xo3kugp7r",
+        },
+      })
+      .get("/users/" + this.$store.state.uid + "/image")
+      .then((res) => {
+        console.log(res);
+      });
+  },
+};
 </script>
 
 <style scoped>
