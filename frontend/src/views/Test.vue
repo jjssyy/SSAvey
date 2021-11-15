efe5c3aebd99577240dd5171c0dbca51a8c5b91e
모진혁 <gkgk246@naver.com>
2021-11-15 15:11:55 +0900

    [S05P31C105-89][FE]feat:워드클라우드 구현

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -8,13 +8,34 @@
       </template>
       <MySurveyAnswer></MySurveyAnswer>
     </v-dialog>
+    <vue-word-cloud
+      style="
+          height: 550px;
+          width: 450px;
+          margin-left: 30px;
+          cursor: pointer;
+        "
+      :words="words"
+      :color="color"
+      font-family="Roboto"
+      font-weight="Bold"
+      :font-size-ratio="Ratio"
+      :rotation="rotation"
+    >
+    </vue-word-cloud>
+    <button @click="asd">버튼</button>
   </v-app>
 </template>
 <script>
 import MySurveyAnswer from '@/components/SurveyResult/MySurveyAnswer.vue'
+import VueWordCloud from 'vuewordcloud'
+let Chance = require('chance')
+let chance = new Chance()
+
 export default {
   components: {
     MySurveyAnswer,
+    VueWordCloud,
   },
   props: {},
   data: () => ({
@@ -108,7 +129,93 @@ export default {
         writer: 'eg41qb97a7bfpfrrc5qga7e46c',
       },
     ],
+    words: [
+      ['한글', 3],
+      ['이미', 3],
+      ['test', 1],
+      ['test', 1],
+      ['test', 1],
+      ['test', 1],
+      ['test', 1],
+      ['test', 1],
+    ],
+    Ratio: 5,
+    colorItems: [
+      [
+        '#FEDE00',
+        '#B4F8C8',
+        '#6AB8EE',
+        '#ff4e69',
+        '#BCECE0',
+        '#4C5270',
+        '#F652A0',
+        '#36EEE0',
+        '#461e47',
+        '#DB1F48',
+      ],
+    ],
+    rotationItemIndex: undefined,
+    rotationItems: [
+      {
+        value: 0,
+      },
+      {
+        value: function() {
+          let chance = new Chance()
+          return chance.pickone([0, 3 / 4])
+        },
+      },
+      {
+        value: function() {
+          let chance = new Chance()
+          return chance.pickone([0, 1 / 8, 3 / 4, 7 / 8])
+        },
+      },
+    ],
+    temp: ['aa', 'bb', 'aa', 'a', 'c'],
+    temp2: [],
   }),
-  methods: {},
+  computed: {
+    // colorItem에 있는 칼라를 랜덤으로 추출해서 바인딩
+    color() {
+      const colors = this.colorItems[0]
+      return function() {
+        return chance.pickone(colors)
+      }
+    },
+    // rotationItem에 있는 각도를 랜덤으로 추출해서 바인딩
+    rotation() {
+      const item = this.rotationItems[this.rotationItemIndex]
+      return item.value
+    },
+  },
+  created() {
+    // 단어 구성 형태 랜덤 선택(십자, 원형)
+    this.rotationItemIndex = chance.integer({
+      min: 0,
+      max: this.rotationItems.length - 1,
+    })
+  },
+  methods: {
+    asd() {
+      console.log(this.temp)
+      for (var i = 0; i < this.temp.length; i++) {
+        var temp = undefined
+        for (var j = 0; j < this.temp2.length; j++) {
+          if (this.temp[i] == this.temp2[j][0]) {
+            console.log(123)
+            temp = j
+          }
+        }
+        if (temp == undefined) {
+          this.temp2.push([this.temp[i], 1])
+        } else {
+          this.temp2[temp][1]++
+        }
+      }
+      this.words = this.temp2
+      console.log(this.temp2)
+    },
+  },
 }
 </script>
